30806fc6e7f6b5d6cb965b1a179d1b817c59c86e
Rodrigo de Sousa <rodrigo.ross.comp@gmail.com>
2022-12-26 20:46:56 -0400

    aula 3 - adicionando stripe

--- nuxt-checkout/nuxt.config.js
+++ nuxt-checkout/nuxt.config.js
@@ -7,12 +7,12 @@ export default {
       lang: 'en'
     },
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: ''}
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css',
         integrity: 'sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6', crossOrigin: 'anonymous'
@@ -41,7 +41,7 @@ export default {
     '@nuxtjs/axios',
 
     ['nuxt-stripe-module', {
-      publishableKey: 'pk_test_51H0wSsFHUJ5mamKON9UhPL8Rws5mF3p5aTqKK9kh2Rvk0DnQcpSLYujzQoWqTyp02tkWjYB8o9YRKSFWThARxyT100uqDDnoVy',
+      publishableKey: 'pk_test_51MJPMWIRvKR1G8onDHR0zd2bA2FNIKAtATdwpnx2LO0UF1KcUkWOqSbFqgbXK4kUy6ZI8nXb6ENE3MYb5QLMDMcC004kM8V2F1',
     }],
   ],
 
